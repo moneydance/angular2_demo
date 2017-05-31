ffd8da4934b4a2f390aa3f1c2b8c28d4b0b40a46
Ben Pusey <Benjamin.Pusey@fmr.com>
2017-05-31 10:14:10 -0400

    getting webpack working and splitting up configs

---
+++ packages/app/config/base.config.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+
+export class BaseConfig {
+	public constructor() {};
+	public get tsconfig():any {
+		const tsconfigFilePath = process.env.APP_ROOT + 'tsconfig.json';
+		return JSON.parse(fs.readFileSync(tsconfigFilePath, 'utf8'));
+	};
+	public paths:any = {
+		ts: {
+			path: 'src/**/*.ts',
+		},
+		entry: {
+			path: ['src/Greeter/Greeter.ts'],
+		},
+		dist: {
+			path: 'dist/',
+		},
+		doc: {
+			path: 'doc/'
+		}
+	};
+	public tslint:any = {
+		formatter: 'stylish'
+	};
+	public rimraf:any = {
+		force: true
+	};
+	public typedoc:any = {
+		module: this.tsconfig.module,
+		target: this.tsconfig.target,
+		includeDeclarations: true,
+		excludeExternals: true,
+		out: this.paths.doc.path,
+		hideGenerator: true,
+		name: 'Sidious App docs'
+	};
+	public webpack:any = {
+		watch: true,
+		devtool: 'source-map',
+		resolve: {
+			extensions: ['.ts', '.tsx'],
+		},
+		/* Documentation https://webpack.github.io/docs/loaders.html#introduction
+		 * Please Note that loaders are applied in right to left order (I know really silly).
+		 */
+		module: {
+			loaders: [
+				{test: /.tsx?$/, loader: ['angular2-template-loader!awesome-typescript-loader'},
+			]
+		}
+	};
+}
--- packages/app/gulpfile.ts
+++ packages/app/gulpfile.ts
@@ -1,92 +1,43 @@
 import {Gulpclass, Task, SequenceTask} from 'gulpclass/Decorators';
-import tslint from 'gulp-tslint';
 import * as gulp from 'gulp';
-import * as typescript from 'gulp-tsc';
 import * as rimraf from 'gulp-rimraf';
 import * as typedoc from 'gulp-typedoc';
+import * as webpack from 'webpack-stream';
+import * as named from 'vinyl-named';
+
+import {BaseConfig as Config} from './config/base.config';
+const config = new Config();
 
 @Gulpclass()
 export class Gulpfile {
-	private static readonly CONFIG: any =
-	{
-		paths: {
-			ts: {
-        		path: 'src/**/*.ts',
-			},
-			stat: {
-				path: ['src/**/*', '!src/**/*.ts'],
-      		},
-      		dist: {
-        		path: 'dist/',
-        		options: {
-          			base: 'src'
-        		},
-			},
-			test: {
-				path: 'dist/**/*.test.js'
-			}
-		},
-		tslint: {
-			formatter: 'stylish'
-		},
-		rimraf: {
-			force: true
-		},
-		typedoc: {
-			module: 'commonjs',
-			target: 'es5',
-			includeDeclarations: true,
-			excludeExternals: true,
-			out: 'doc/',
-			hideGenerator: true,
-			name: 'Sidious Super Cool Angular2 App Demo'
-		}
-	};
-
 	/**
 	 * Task to clean the dist folder by running force rm -rf on it.
 	 */
 	@Task('clean')
 	clean() {
-		return gulp.src([Gulpfile.CONFIG.paths.dist.path])
-			.pipe(rimraf(Gulpfile.CONFIG.rimraf));
+		return gulp.src(config.paths.dist.path)
+			.pipe(rimraf(config.rimraf));
 	}
 
-	/**
-	 * Task to run lint formatting on src code
-	 */
-	@Task('lint-src')
-	lintSrc() {
-		return gulp.src(Gulpfile.CONFIG.paths.ts.path)
-			.pipe(tslint(Gulpfile.CONFIG.tslint))
-			.pipe(tslint.report());
-	}
 
 	/**
 	 * Task to compile typescript code in src
 	 */
-	@Task('tsc', ['lint-src'])
+	@Task('webpack')
 	tsc() {
-		return gulp.src(Gulpfile.CONFIG.paths.ts.path, Gulpfile.CONFIG.paths.dist.options)
-			.pipe(typescript())
-			.pipe(gulp.dest(Gulpfile.CONFIG.paths.dist.path));
+		return gulp.src(config.paths.entry.path)
+			.pipe(named())
+			.pipe(webpack(config.webpack));
+			.pipe(gulp.dest(config.paths.dist.path));
 	}
 
-	/*
-	 * Task to move static files over to the dist
-	 */
-	@Task('mv-static-src', ['lint-src'])
-	mvStaticSrc() {
-		return gulp.src(Gulpfile.CONFIG.paths.stat.path, Gulpfile.CONFIG.paths.dist.options)
-			.pipe(gulp.dest(Gulpfile.CONFIG.paths.dist.path));
-	}
 
 	/**
 	 * Task to compile code base
 	 */
 	@SequenceTask('compile')
 	compile() {
-		return ['clean', ['tsc', 'mv-static-src']];
+		return ['clean', 'webpack'];
 	}
 
 	/**
@@ -94,8 +45,8 @@ export class Gulpfile {
 	 */
 	@Task('doc')
 	doc() {
-		return gulp.src(Gulpfile.CONFIG.paths.ts.path)
-			.pipe(typedoc(Gulpfile.CONFIG.typedoc));
+		return gulp.src(config.paths.ts.path)
+			.pipe(typedoc(config.typedoc));
 	}
 
 	/**
