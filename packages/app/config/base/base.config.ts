7f662ad9c837c3d1893596194fe5f07900750d0b
Benajamin Pusey <bpusey@bu.edu>
2017-07-01 22:10:04 -0400

    worked on webpack and dockerizing

--- packages/app/config/base/base.config.ts
+++ packages/app/config/base/base.config.ts
@@ -14,25 +14,41 @@ export class BaseConfig {
   constructor() {
     this.baseDir = baseDir;
     this.paths = {
-      entry: { path: [path.join(baseDir, 'src/Greeter/Greeter.ts')] },
-      dist: { path: path.join(baseDir, 'dist/') },
-      doc: { path: path.join(baseDir, 'src/') },
-      tsconfig: { path: path.join(baseDir, 'tsconfig.json') }
+      entry: { path: [path.join(this.baseDir, 'src/Greeter/Greeter.ts')] },
+      dist: { path: path.join(this.baseDir, 'dist/') },
+      doc: { path: path.join(this.baseDir, 'src/') },
+      tsconfig: { path: path.join(this.baseDir, 'tsconfig.json') }
     };
     this.tsconfig = JSON.parse(fs.readFileSync(this.paths.tsconfig.path, 'utf8'));
     this.tslint = { formatter: 'stylish' };
     this.rimraf = { force: true };
     this.webpack = {
+      context: this.baseDir,
       watch: true,
       devtool: 'inline-source-map',
       resolve: { extensions: ['', '.json', '.ts', '.tsx'] },
       module: {
         loaders: [
           {
-            test: /.tsx?$/,
+            test: /\.tsx?$/,
             loader: 'angular2-template-loader!awesome-typescript-loader',
             options: { configFileName: this.paths.tsconfig.path }
-          }
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeAttributeQuotes: false,
+              caseSensitive: true,
+              customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
+              customAttrAssign: [ /\)?\]?=/ ]
+            }
+          },
+          {
+            test: /\.scss$/,
+            loader: 'style-loader!css-loader!sass-loader',
+          },
         ]
       },
     };
